dd02631af748f5608771a8bfd4be137b7a5d0269
Mariachauhdhary-code <127111758+Mariachauhdhary-code@users.noreply.github.com>
2023-03-08 05:26:23 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+
+import { View, Text } from 'react-native';
+import React from 'react';
+import Splash1 from './assets/screens/splashscreens/Splash1';
+import Splash2 from './assets/screens/splashscreens/Splash2';
+import Splash3 from './assets/screens/splashscreens/Splash3';
+import Login from './assets/screens/SigninSignup/Login';
+import Register from './assets/screens/SigninSignup/Register';
+import ForgotPassword from './assets/screens/SigninSignup/ForgotPassword';
+import NewPassword from './assets/screens/SigninSignup/NewPassword';
+import PasswordChanged from './assets/screens/SigninSignup/PasswordChanged';
+import MainScreen1 from './assets/screens/mainscreen/MainScreen1';
+import MainHeader from './assets/components/MainHeader';
+
+
+const App = () => {
+  return (
+    <View>
+      {/* <Splash1/> */}
+      {/* <Splash2/> */}
+      {/* <Splash3/> */}
+
+
+
+
+      {/* <Login/> */}
+      {/* <Register/> */}
+      {/* <ForgotPassword/> */}
+      {/* <NewPassword/> */}
+      {/* <PasswordChanged/> */}
+      <MainScreen1/>
+
+    </View>
+
+  )
+}
+export default App;
